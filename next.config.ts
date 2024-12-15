c205d9fef12e2b0579a6fd9f2141f0dd23d5877b
Rohbunny01 <matias.beltran08@inacapmail.cl>
2024-12-15 01:39:15 -0300

    actualización de dependencias del frontend

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
 			},
 			{
 				protocol: 'https',
-				hostname: 'deploy-pos-elolimpo-nestjs.onrender.com',
+				hostname: "deploy-pos-elolimpo-nestjs.onrender.com",
 			},
 		],
 	},
